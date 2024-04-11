e7c6e6fed4459cfc65852646cc2eebb5989afe20
xfshahrukh44 <ssidduit@gmail.com>
2024-04-12 00:38:31 +0500

    @getimagesize

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,9 +94,9 @@
                             <div class="product-img-action-wrap mb-10">
                                 <div class="product-img product-img-zoom">
                                     <a href="{{ route('shopDetail', ['id' => $products->id, 'name' => preg_replace('/[^A-Za-z0-9\-]/', '', strtolower(str_replace(' ', '-', $products->product_title))) ]) }}">
-                                        @if (@getimagesize($products->thumb_image ) != false)
+                                        @if ($products->thumb_image != '' && @getimagesize($products->thumb_image ) != false)
                                             <img class="default-img" src="{{ asset($products->thumb_image ) }}" alt="">
-                                        @elseif (@getimagesize($products->image ) != false)
+                                        @elseif ($products->image != '' && @getimagesize($products->image ) != false)
                                             <img class="default-img" src="{{ asset($products->image ) }}" alt="">
                                         @else
                                             <img class="default-img" src="{{ asset('uploads/products/no_image.jpg') }}" alt="">
